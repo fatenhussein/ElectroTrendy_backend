ef888e0c8cf67961499bf2b5281b0f8b51b98ea9
Ibrahim-AbuFarha <ibrahim.j.abufarha@gmail.com>
2023-07-27 01:02:26 +0300

    cartController

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,42 +1,78 @@
 const Cart = require('../models/cartModel');
 
-// 
-
-// /cart/addItem/:id
-
-// {quantity ,customerId }
-
+// add to cart
 exports.addItemToCart = (req, res) => {
   try {
     const { quantity, customerId } = req.body;
     const { productId } = req.params;
+    // find the user cart  Cart.find({customerId })
+    const userCart = Cart.find({ customerId });
+    let product = userCart.productList.find(
+      (item) => item.productId === productId
+    );
+    if (!product) {
+      product = userCart.productList.push({ quantity, productId });
+    } else {
+      product = userCart.productList.map((item) => {
+        if (item.productId === productId) {
+          item.quantity += 1;
+        }
+      });
+    }
+    res.status(201).json({
+      message: 'success',
+      data: { product },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//delete 
+exports.deleteItemFromCart = (req, res) => {
+  try {
+    const { customerId } = req.body;
+    const { productId } = req.params;
+    // find the user cart  Cart.find({customerId })
+    const userCart = Cart.find({ customerId });
+    let products = userCart.productList.filter(
+      (item) => item.productId !== productId
+    );
 
     res.status(201).json({
       message: 'success',
-      data: { newCart },
+      data: { products },
     });
   } catch (err) {
     console.log(err);
   }
 };
 
-// add to cart
+exports.removeItemFromCart = (req, res) => {
+  try {
+    const { customerId } = req.body;
+    const { productId } = req.params;
+    // find the user cart  Cart.find({customerId })
+    const userCart = Cart.find({ customerId });
+    let product = userCart.productList.find(
+      (item) => item.productId === productId
+    );
+    if (product.quantity > 1) {
+      product = userCart.productList.map((item) => {
+        if (item.productId === productId) {
+          item.quantity -= 1;
+        }
+      });
+    } else {
+      product = userCart.productList.filter(
+        (item) => item.productId !== productId
+      );
+    }
 
-// find the user cart  Cart.find({customerId })
-
-// userCart = {id :65465498 ,  customerId : 45 , cartItems : []}
-
-// const product =  userCart.cartItems.find((item) => item.productId === productId)
-
-// if(product) {
-//
-//product.quantity+=quantity;
-//}else{
-//userCart.cartItems.push({productId , quantity})
-// }
-
-
-
-//handel add item to cart before he signed in 
-//-----------------------------------------------------
-//add to cart/ delete from cart/get cart / routes 
+    res.status(201).json({
+      message: 'success',
+      data: { product },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const Router = express.Router();
 
-const {} = require('../controllers/cartController');
+const {
+  addItemToCart,
+  deleteItemFromCart,
+  removeItemFromCart,
+} = require('../controllers/cartController');
 
 Router.route('/').get();
-Router.route('/addItem/:id').patch();
-Router.route('/deleteItem/:id').patch();
+Router.route('/addItem/:id').patch(addItemToCart);
+Router.route('/deleteItem/:id').patch(deleteItemFromCart);
+Router.route('/removeItem/:id').patch(removeItemFromCart);
 
 module.exports = Router;
