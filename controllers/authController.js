376c39d9bfc40d45d67f54bb133d6a84d6286793
Ibrahim-AbuFarha <ibrahim.j.abufarha@gmail.com>
2023-07-30 01:01:49 +0300

    login handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,16 +45,22 @@ exports.signIn = async (req, res) => {
       throw new Error('Please provide your email and password');
     }
     //2)Check if user exist && password is correct
-    const currentUser = await Customer.findOne({ email });
-
+    const currentUser = await Customer.findOne({ email }).select('+password');
+    console.log(currentUser);
     //3)If everything ok, send token to client
-
-    const token = jwt.sign({ id: currentUser._id }, process.env.JWT_SECRET);
-    res.status(200).json({
-      status: 'success',
-      token,
-      id: currentUser._id,
-    });
+    if (currentUser.password === password) {
+      const token = jwt.sign({ id: currentUser._id }, process.env.JWT_SECRET);
+      res.status(200).json({
+        status: 'success',
+        token,
+        id: currentUser._id,
+      });
+    } else {
+      res.status(404).json({
+        status: 'fail',
+        message: 'ok',
+      });
+    }
   } catch (err) {
     res.status(404).json({
       status: 'fail',
